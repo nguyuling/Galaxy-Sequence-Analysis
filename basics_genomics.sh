2cefebab289ea3e55511029f0b31d978cb3c7778
nguyuling <nguyuling@gmail.com>
2026-09-11 15:21:07 +0800

    simplify script and comment

--- basics_genomics.sh
+++ basics_genomics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# define file paths as variables
+# 1. define file paths as variables
 REPO="/Users/nguyuling/Galaxy-Sequence-Analysis"
 EXONS="$REPO/datasets/4104428/UCSC-hg38-chr22-Coding-Exons.bed"
 SNPS="$REPO/datasets/4104428/UCSC-hg38-chr22-dbSNP153-Whole-Gene-SNPs.bed"
@@ -10,21 +10,18 @@ SNPS_COUNTS_SORTED="$REPO/basics-genomics/snps_counts_per_exon_sorted.bed"
 TOP5_EXONS_SNPS_COUNTS="$REPO/basics-genomics/top5_exons_snps_counts.bed"
 TOP5_EXONS="$REPO/basics-genomics/top5_exons.bed"
 
-# bedtools intersect
-bedtools intersect \
-  -a "$EXONS" \
-  -b "$SNPS" \
-  -wa -wb > "$INTERSECT"
+# 2. bedtools intersect
+bedtools intersect -a "$EXONS" -b "$SNPS" -wa -wb > "$INTERSECT"
 
-# datamash: group by column 4, count unique values in column 10
+# 3. datamash: group by column 4, count unique values in column 10
 datamash -s -g 4 countunique 10 < "$INTERSECT" > "$SNPS_COUNTS"
 
-# sort: by column 2, descending (-r), fast numeric sort (-n)
+# 4. sort: by column 2, descending (-r), fast numeric sort (-n)
 sort -k2,2rn "$SNPS_COUNTS" > "$SNPS_COUNTS_SORTED"
 
-# select first: top 5 rows
+# 5. select first: top 5 rows
 head -n 5 "$SNPS_COUNTS_SORTED" > "$TOP5_EXONS_SNPS_COUNTS"
 
-# compare two datasets
+# 6. compare two datasets
 awk 'NR==FNR {ids[$1]; next} $4 in ids' "$TOP5_EXONS_SNPS_COUNTS" "$EXONS" > "$TOP5_EXONS"
 cat "$TOP5_EXONS"
