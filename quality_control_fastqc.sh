b0f5490f8cbe7b34b298a65e0d88549193e452bf
nguyuling <nguyuling@gmail.com>
2026-09-15 13:47:53 +0800

    delete fastq right after done qc

--- quality_control_fastqc.sh
+++ quality_control_fastqc.sh
@@ -38,6 +38,4 @@ fastqe "$FASTQ_TRIMMED" --output "$FASTQE_TRIMMED"
 # 5. clean up downloaded fastq
 rm -f \
     "$FASTQ_LOCAL" \
-    "$FASTQ_TRIMMED" \
-    "$REPO/female_oral2.fastq-4143_fastqc.zip" \
-    "$REPO/female_oral2.fastq-4143_fastqc.html"
+    "$FASTQ_TRIMMED"
