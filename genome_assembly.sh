b68621f8f7ef1bef02e0f463d392133bc44b0bf5
nguyuling <nguyuling@gmail.com>
2026-09-17 14:09:38 +0800

    use python to combine paired end reads to interlaced and then feed to velvetg for assembly, use quast to compare velvetg and spades

--- genome_assembly.sh
+++ genome_assembly.sh
@@ -7,9 +7,10 @@ DATA_DIR="$REPO/1_Datasets"
 FASTQC_DIR="$REPO/2_FastQC"
 MULTIQC_DIR="$REPO/3_MultiQC"
 SEQKIT_DIR="$REPO/4_Seqkit_InterleavePairedEnd"
+VELVET_DIR="$REPO/5_Velvet_GenAssembly"
 SPADES_DIR="$REPO/5_Spades_GenAssembly"
 QUAST_DIR="$REPO/6_Quast_AssemblyAssessment"
-mkdir -p "$REPO" "$DATA_DIR" "$FASTQC_DIR" "$MULTIQC_DIR" "$SEQKIT_DIR" "$SPADES_DIR" "$QUAST_DIR"
+mkdir -p "$REPO" "$DATA_DIR" "$FASTQC_DIR" "$MULTIQC_DIR" "$SEQKIT_DIR" "$VELVET_DIR" "$SPADES_DIR" "$QUAST_DIR"
 
 R1_URL="https://zenodo.org/record/582600/files/mutant_R1.fastq"
 R2_URL="https://zenodo.org/record/582600/files/mutant_R2.fastq"
@@ -17,6 +18,7 @@ WT_URL="https://zenodo.org/record/582600/files/wildtype.fna"
 R1_LOCAL="$DATA_DIR/R1.fastq"
 R2_LOCAL="$DATA_DIR/R2.fastq"
 WT_LOCAL="$DATA_DIR/wildtype.fna"
+INTERLACED_MUTANT="$SEQKIT_DIR/interlaced_mutant.fastq"
 
 echo "1. Downloading the datasets..."
 curl -L -C - --retry 5 --retry-connrefused -o "$R1_LOCAL" "$R1_URL"
@@ -30,12 +32,26 @@ echo "3. Running MultiQC on the paired-end reads' FastQC raw data..."
 multiqc "$FASTQC_DIR" -o "$MULTIQC_DIR" -n "multiqc_report.html"
 
 echo "4. Combining paired-end FASTQ files into an interleaving file..."
-seqkit pair \
-    -1 "$R1_LOCAL" \
-    -2 "$R2_LOCAL" \
-    -O "$SEQKIT_DIR"
+python3 -c '
+import sys
+r1_path = "'"$R1_LOCAL"'"
+r2_path = "'"$R2_LOCAL"'"
+out_path = "'"$INTERLACED_MUTANT"'"
+
+with open(r1_path) as f1, open(r2_path) as f2, open(out_path, "w") as out:
+    while True:
+        r1_lines = [f1.readline() for _ in range(4)]
+        r2_lines = [f2.readline() for _ in range(4)]
+        if not r1_lines[0] or not r2_lines[0]:
+            break
+        out.writelines(r1_lines + r2_lines)
+'
 
 echo "5. Assembling genome from short reads..."
+velveth "$VELVET_DIR" 29 -fastq -shortPaired "$INTERLACED_MUTANT"
+velvetg "$VELVET_DIR" -clean yes
+
+echo "5. Combining paired-end FASTQ files into an interleaving file..."
 spades.py \
     -1 "$R1_LOCAL" \
     -2 "$R2_LOCAL" \
@@ -43,6 +59,7 @@ spades.py \
 
 echo "6. Assessing quality of genome assembly..."
 quast.py \
+    "$VELVET_DIR/contigs.fa" \
     "$SPADES_DIR/contigs.fasta" \
     -r "$WT_LOCAL" \
     -o "$QUAST_DIR" \
