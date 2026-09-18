109b26b6759ec78de27b33a14ab48780840c378d
nguyuling <nguyuling@gmail.com>
2026-09-18 11:39:01 +0800

    change output dir to hard drive

--- basics_genomics.sh
+++ basics_genomics.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 
-# 1. define file paths as variables
-REPO="/Users/nguyuling/Galaxy-Sequence-Analysis"
-EXONS="$REPO/datasets/4104428/UCSC-hg38-chr22-Coding-Exons.bed"
-SNPS="$REPO/datasets/4104428/UCSC-hg38-chr22-dbSNP153-Whole-Gene-SNPs.bed"
-INTERSECT="$REPO/basics-genomics/exon_snps_intersect.bed"
-SNPS_COUNTS="$REPO/basics-genomics/snps_counts_per_exon.bed"
-SNPS_COUNTS_SORTED="$REPO/basics-genomics/snps_counts_per_exon_sorted.bed"
-TOP5_EXONS_SNPS_COUNTS="$REPO/basics-genomics/top5_exons_snps_counts.bed"
-TOP5_EXONS="$REPO/basics-genomics/top5_exons.bed"
+REPO="/Volumes/T7/270918_basics_of_genomics"
+DATA_DIR="$REPO/1_Datasets"
+BEDTOOLS_DIR="$REPO/2_Bedtools_FindIntersection"
+DATAMESH_DIR="$REPO/3_Datamesh_GroupByExonID"
+SORT_DIR="$REPO/4_Sort"
 
-# 2. bedtools intersect: find intersection between exons and snps
-bedtools intersect -a "$EXONS" -b "$SNPS" -wa -wb > "$INTERSECT"
+mkdir "$REPO" "$DATA_DIR" "$BEDTOOLS_DIR" "$DATAMESH_DIR" "$SORT_DIR"
+
+EXONS_URL="https://zenodo.org/record/4104428/files/UCSC-hg38-chr22-Coding-Exons.bed"
+SNPS_URL="https://zenodo.org/record/4104428/files/UCSC-hg38-chr22-dbSNP153-Whole-Gene-SNPs.bed"
+EXONS_LOCAL="$DATA_DIR/exons.bed"
+SNPS_LOCAL="$DATA_DIR/snps.bed"
+INTERSECT="$BEDTOOLS_DIR/exon_snps_intersect.bed"
+SNPS_COUNTS="$DATAMESH_DIR/snps_counts_per_exon.bed"
+SNPS_COUNTS_SORTED="$SORT_DIR/snps_counts_per_exon_sorted.bed"
+TOP5_EXONS_SNPS_COUNTS="$SORT_DIR/top5_exons_snps_counts.bed"
+TOP5_EXONS="$SORT_DIR/top5_exons.bed"
+
+echo "1. Downloading exons and SNPs datasets..."
+curl -L -C - --retry 5 --retry-connrefused -o "$EXONS_LOCAL" "$EXONS_URL"
+curl -L -C - --retry 5 --retry-connrefused -o "$SNPS_LOCAL" "$SNPS_URL"
+
+echo "2. Finding intersection between exons and SNPs..."
+bedtools intersect -a "$EXONS_LOCAL" -b "$SNPS_LOCAL" -wa -wb > "$INTERSECT"
 # -wa write the record from exons (file a)
 # -wb append the overlapping record from snps (file b) to the output file.
 
-# 3. datamash: group by column 4, count unique values in column 10
+echo "3. Grouping by exon ID, then counting unique SNPs of each exon ID..."
 datamash -s -g 4 countunique 10 < "$INTERSECT" > "$SNPS_COUNTS"
 # -s sort the input by the grouping column exon id (required)
 # -g 4 group records based on the exon id (column 4)
 # countunique 10 count number of snps (column 10) per exon
 
-# 4. sort: by column 2, descending (-r), fast numeric sort (-n)
+echo "4. Sorting by SNPs count in descending order..."
 sort -k2,2rn "$SNPS_COUNTS" > "$SNPS_COUNTS_SORTED"
 # -k2,2 sort by key snps count (column 2)
 # r reverse the sort order to descending
 # n use numeric sorting
 
-# 5. select first: top 5 rows
+echo "5. Selecting top 5 exons with most SNPs..."
 head -n 5 "$SNPS_COUNTS_SORTED" > "$TOP5_EXONS_SNPS_COUNTS"
 # -n 5 extract top 5 lines of records
 
-# 6. compare two datasets
-awk 'NR==FNR {ids[$1]; next} $4 in ids' "$TOP5_EXONS_SNPS_COUNTS" "$EXONS" > "$TOP5_EXONS"
+echo "6. Recovering exons' data in other columns..."
+awk 'NR==FNR {ids[$1]; next} $4 in ids' "$TOP5_EXONS_SNPS_COUNTS" "$EXONS_LOCAL" > "$TOP5_EXONS"
 cat "$TOP5_EXONS"
 # NR==FNR number of records (cumulative records across files) only equal to file number of records (reset to 1 when reading a new file) when reading file 1
 # ids[$1] at file 1, take the exon id (column 1) as the key in a hash map
 # next skip the rest of the file and move to next line, to avoid running the script on the rest of the columns
 # $4 in ids` once NR!=FNR when moving to file 2, check if the exon id (column 4) exists in hash map, if yes then write to output file
+
+echo "Completed searching for top 5 exons with most SNPs in Chromosome 22!"
--- mapping.sh
+++ mapping.sh
@@ -2,38 +2,37 @@
 
 set -e
 
-# Harddrive paths
-HARD_DRIVE="/Volumes/T7"
-DATA_DIR="$HARD_DRIVE/mapping"
-MM10_DIR="$HARD_DRIVE/mouse_mm10"
-mkdir -p "$DATA_DIR"
+REPO="/Volumes/T7/270916_mapping"
+DATA_DIR="$REPO/1_Datasets"
+MM10_DIR="/Volumes/T7/mouse_mm10"
+ALIGNMENT_DIR="$REPO/2_Alignment"
+FILTERED_DIR="$REPO/3_FilteredAlignment"
 
-# input and output paths on hard drive
+mkdir -p "$REPO" "$DATA_DIR" "$ALIGNMENT_DIR" "$FILTERED_DIR"
+
 R1_URL="https://zenodo.org/record/1324070/files/wt_H3K4me3_read1.fastq.gz"
 R2_URL="https://zenodo.org/record/1324070/files/wt_H3K4me3_read2.fastq.gz"
 R1_LOCAL="$DATA_DIR/R1.fastq.gz"    
 R2_LOCAL="$DATA_DIR/R2.fastq.gz"
-RAW_BAM="$DATA_DIR/raw_alignment.bam"
-FILTERED_BAM="$DATA_DIR/filtered_alignment.bam"
-RAW_STATS="$DATA_DIR/raw_alignment_stats.txt"
-FILTERED_STATS="$DATA_DIR/filtered_alignment_stats.txt"
+ALIGNMENT="$ALIGNMENT_DIR/raw_alignment.bam"
+ALIGNMENT_STATS="$ALIGNMENT_DIR/raw_alignment_stats.txt"
+FILTERED="$FILTERED_DIR/filtered_alignment.bam"
+FILTERED_STATS="$FILTERED_DIR/filtered_alignment_stats.txt"
 MM10_FA_LOCAL="$MM10_DIR/mm10.fa.gz"
 INDEX_PREFIX="$MM10_DIR/mm10_index"
 
-# 1. download raw fastq datasets
-echo "1. Downloading raw paired FASTQ reads..."
+echo "1. Downloading paired-end FASTQ reads..."
 curl -L -C - --retry 5 --retry-connrefused -o "$R1_LOCAL" "$R1_URL"
 curl -L -C - --retry 5 --retry-connrefused -o "$R2_LOCAL" "$R2_URL"
 
-# 2. check and build mm10 reference index (if not exist)
+echo "2. Building mm10 reference index (if not exist)..."
 if [ ! -f "${INDEX_PREFIX}.1.bt2" ]; then
-    echo "2. Index not found. Building Bowtie2 index in $MM10_DIR..."
+    echo "Index not found. Building Bowtie2 index in $MM10_DIR..."
     bowtie2-build "$MM10_FA_LOCAL" "$INDEX_PREFIX"
 else
-    echo "2. Reusing existing mm10 Bowtie2 index from $MM10_DIR..."
+    echo "Reusing existing mm10 Bowtie2 index from $MM10_DIR..."
 fi
 
-# 3. map paired-end reads using bowtie2 and stream output to bam
 echo "3. Mapping paired-end reads with Bowtie2..."
 bowtie2 \
     -x "$INDEX_PREFIX" \
@@ -41,22 +40,15 @@ bowtie2 \
     -2 "$R2_LOCAL" \
     --no-mixed \
     --no-discordant \
-    | samtools view -bS - > "$RAW_BAM"
+    | samtools view -bS - > "$ALIGNMENT"
 
-# 4. generate stats for raw bam alignment
-echo "4. Generating stats for raw BAM..."
-samtools stats "$RAW_BAM" > "$RAW_STATS"
+echo "4. Generating stats for alignment BAM..."
+samtools stats "$ALIGNMENT" > "$ALIGNMENT_STATS"
 
-# 5. filter bam file for properly paired and high-quality mapped reads
-echo "5. Filtering BAM file..."
-samtools view -b -F 4 -F 8 -f 2 -q 30 "$RAW_BAM" > "$FILTERED_BAM"
+echo "5. Filtering the alignment BAM..."
+samtools view -b -F 4 -F 8 -f 2 -q 30 "$ALIGNMENT" > "$FILTERED"
 
-# 6. generate stats for filtered bam alignment
-echo "6. Generating stats for filtered BAM..."
-samtools stats "$FILTERED_BAM" > "$FILTERED_STATS"
+echo "6. Generating stats for the filtered alignment BAM..."
+samtools stats "$FILTERED" > "$FILTERED_STATS"
 
-# 7. clean up temporary fastq files
-echo "7. Cleaning up raw FASTQ files..."
-rm -f "$R1_LOCAL" "$R2_LOCAL"
-
-echo "Pipeline finished! Output summary stats generated on T7."
+echo "Completed genome mapping!"
